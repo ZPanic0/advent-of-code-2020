76ca4479352e1355beaa17faaa1b2817ca5b4447
James Gregor <jgregor@fleetstreetusa.com>
2020-12-01 09:49:10 -0500

    Add 1.1 solution

---
+++ 1.1/index.js
@@ -0,0 +1,23 @@
+const input = require('fs')
+    .readFileSync('./input.txt')
+    .toString()
+    .split('\n')
+    .map(Number)
+    .sort((x, y) => y - x)
+
+const target = 2020
+
+const scan = () => {
+    for (let aIndex = 0; aIndex < input.length; aIndex++) {
+        for (let bIndex = input.length - 1; bIndex > 0; bIndex--) {
+            const sum = input[aIndex] + input[bIndex]
+            if (sum > target) break
+            if (sum === target) return [input[aIndex], input[bIndex]]
+        }
+    }
+    throw new Error('No match')
+}
+
+const [firstDigit, secondDigit] = scan()
+
+console.log(firstDigit * secondDigit)
