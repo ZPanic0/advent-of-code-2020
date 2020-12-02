05781ad79f217b3d320f694de4fa369447c2f158
James Gregor <jgregor@fleetstreetusa.com>
2020-12-02 13:48:55 -0500

    Add 2.2 solution

---
+++ 2.2/index.js
@@ -0,0 +1,21 @@
+const inputs = require('fs')
+    .readFileSync('./input.txt')
+    .toString()
+    .split('\n')
+
+//Removing empty entry from the end caused by splitting on newline
+inputs.length--
+
+let validCount = 0
+
+for (let input of inputs) {
+    let [range, character, password] = input.split(' ')
+    character = character.slice(0, 1)
+    const [firstIndex, secondIndex] = range.split('-').map(Number)
+
+    validCount +=
+        password[firstIndex - 1] === character ^
+            password[secondIndex - 1] == character ? 1 : 0
+}
+
+console.log(validCount)
