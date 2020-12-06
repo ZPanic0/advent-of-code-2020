3d5d24824c8a176526c6d9733de99c68b051cdc4
ZPanic0 <zc.sgs5@gmail.com>
2020-12-06 00:29:37 -0500

    Add 6.1 solution

---
+++ 6.1/index.js
@@ -0,0 +1,18 @@
+const { group } = require('console')
+
+const inputs = require('fs')
+    .readFileSync('./input.txt')
+    .toString()
+    .split('\n\n')
+
+let sum = 0
+for (let input of inputs) {
+    const groupAnswers = input
+        .replace(/\n/g, '')
+        .split('')
+        .reduce((accumulator, thisLetter) => accumulator.add(thisLetter), new Set())
+
+    sum += [...groupAnswers].length
+}
+
+console.log(sum)
